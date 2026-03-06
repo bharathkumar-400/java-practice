7e0bf396df5148dbb463acdcff0a7d6045e30b28
bharathkumar-400 <bharathkumarpr17@gmail.com>
2026-03-06 23:16:17 +0530

    Added ArrayPractice program

---
+++ ArrayPractice.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+class ArrayPractice
+
+{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		/*int arr[]=new int[10];
+		
+		arr[9]=20;
+		arr[7]=9;
+		for(int i=0;i<arr.length;i++){
+	
+
+		System.out.println(arr[i]);
+		}*/
+		/*int arr1[]={10,20,30,40};
+		for(int i=0;i<arr1.length;i++){
+	
+
+		System.out.println(arr1[i]);
+		}*/
+		/*String arr2[]=new String[10];
+		arr2[2]="bharath";
+		arr2[6]="naveen";
+		arr2[8]="Darshan";
+		for (int i=0;i<arr2.length ;i++ )
+		{
+		System.out.println(arr2[i]);
+		}*/
+		/*String arr2[]={"bharath","naveen","Darshan"};
+		
+		for (int i=0;i<arr2.length ;i++ )
+		{
+		System.out.println(arr2[i]);
+		}*/
+		String arr2[];
+		System.out.println("enter The array elements");
+		String line=s.nextLine();
+		String arr2[4]=s.nextLine();
+		
+		
+		
+		System.out.println(arr2);
+		
+	}
+}
